44ff39a0f819c13df0763ecc04286553b36445e6
Teddyburgonde <bandama.teddy@gmail.com>
2024-10-29 19:10:38 +0100

    update file excalidraw

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 15:25:13 by tebandam          #+#    #+#             */
-/*   Updated: 2024/10/29 15:46:57 by tebandam         ###   ########.fr       */
+/*   Updated: 2024/10/29 19:09:07 by tebandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,26 @@ ScalarConverter::ScalarConverter(ScalarConverter const &cpy)
 	*this = cpy;
 }
 
-ScalarConverter const & ScalarConverter::operator=(ScalarConverter const &rhs){}
+ScalarConverter const & ScalarConverter::operator=(ScalarConverter const &rhs)
+{
+    return (*this);
+}
 
 void ScalarConverter::convert(std::string const &literal)
 {
 	
 }
+
+
+/*
+
+Conversion vers double :
+
+    Pour un double, tu feras une vérification similaire à celle du float, 
+	mais sans le suffixe f. 
+	Encore une fois, tu devras gérer des littéraux spéciaux comme nan, -inf, et +inf.
+
+Affichage des résultats :
+
+    Une fois les conversions effectuées, tu afficheras les valeurs converties pour chaque type.
+*/
