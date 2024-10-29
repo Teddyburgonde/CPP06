11bc426b47795fe79830b8a92977da41b4a0a9ed
Teddyburgonde <bandama.teddy@gmail.com>
2024-10-29 17:35:09 +0100

    update file excalidraw

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 15:25:13 by tebandam          #+#    #+#             */
-/*   Updated: 2024/10/29 15:41:48 by tebandam         ###   ########.fr       */
+/*   Updated: 2024/10/29 15:46:57 by tebandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,3 +23,8 @@ ScalarConverter::ScalarConverter(ScalarConverter const &cpy)
 }
 
 ScalarConverter const & ScalarConverter::operator=(ScalarConverter const &rhs){}
+
+void ScalarConverter::convert(std::string const &literal)
+{
+	
+}
